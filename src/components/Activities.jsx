a062a90a6d138b99fd1357da6ab8770ff6eee424
min <kotomi.hinahima@gmail.com>
2026-03-24 14:27:33 +0900

    refs: #29 Activitiesコンポーネントを作成

---
+++ src/components/Activities.jsx
@@ -0,0 +1,13 @@
+export default function Activities() {
+  return (
+    <section id="activities">
+      <h2>活動内容</h2>
+      <p>
+        毎週金曜日18:00~ AtCoder会<br />
+        学期ごとのLT会<br />
+        TechTrainを活用した勉強会<br />
+        もくもくと勉強する会<br />
+      </p>
+    </section>
+  );
+}
