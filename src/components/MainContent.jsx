d40257c63d3bc3d426543e363fcd59711b21a44f
min <kotomi.hinahima@gmail.com>
2026-03-27 01:52:59 +0900

    refs: #33 Membersコンポーネントの作成

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useMemo, useRef, useState } from "react";
 import Access from "./Access.jsx";
+import Members from "./Members.jsx";
 
 /* ===== お知らせ ===== */
 function Notice() {
@@ -224,15 +225,7 @@ export default function MainContent() {
         </p>
       </section>
 
-      <section id="members">
-        <h2>メンバー募集</h2>
-        <p>
-          学年学科関係なく常時新規メンバー募集中！<br />
-          年会費は無料！<br />
-          「やってみたい」という興味があれば、経験は問いません。<br />
-          初心者から経験者まで、誰でも歓迎しています。<br />
-        </p>
-      </section>
+      <Members />
 
       <Notice />
       <FAQ />
---
+++ src/components/Members.jsx
@@ -0,0 +1,17 @@
+export default function Members() {
+  return (
+    <section id="members">
+      <h2>メンバー募集</h2>
+      <p>
+        学年学科関係なく常時新規メンバー募集中！
+        <br />
+        年会費は無料！
+        <br />
+        「やってみたい」という興味があれば、経験は問いません。
+        <br />
+        初心者から経験者まで、誰でも歓迎しています。
+        <br />
+      </p>
+    </section>
+  );
+}
